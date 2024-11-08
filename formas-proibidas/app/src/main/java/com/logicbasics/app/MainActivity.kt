e74e3a473db3dfb2c249d47d282a35e8356deab3
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2024-11-08 12:37:23 -0300

    update do som

--- formas-proibidas/app/src/main/java/com/logicbasics/app/MainActivity.kt
+++ formas-proibidas/app/src/main/java/com/logicbasics/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.logicbasics.app
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,15 +11,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
@@ -30,31 +27,23 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.logicbasics.app.ui.theme.FormasproibidasTheme
-import kotlinx.coroutines.android.awaitFrame
+import android.media.MediaPlayer
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -123,10 +112,15 @@ fun Inicio() {
             ) {
                 // MavHost é o componente que gerencia a navegação entre as telas
                 NavHost(navController = navController, startDestination = "login") {
+
                     // Definindo nossas rotas de navegação
-                    composable("login") { ConteudoLogin(navController, innerPadding) }
+                    composable("login") {
+                        val context = LocalContext.current
+                        ConteudoLogin(navController, innerPadding, context)
+                    }
                     composable("home") { Home(navController) }
                     composable("circulo") { ConteudoCirculo(navController) }
+
                 }
             }
         },
@@ -173,7 +167,9 @@ fun Inicio() {
 
 
 @Composable
-fun ConteudoLogin(navController: NavController, innerPadding: PaddingValues) {
+fun ConteudoLogin(navController: NavController, innerPadding: PaddingValues, context: Context) {
+    val mediaPlayer = MediaPlayer.create(context, R.raw.button)
+
     Column {
         Text(
             text = "Bem-vindo ao Portal das Formas"
@@ -195,6 +191,15 @@ fun ConteudoLogin(navController: NavController, innerPadding: PaddingValues) {
             modifier = Modifier.padding(16.dp)
         ) {
             Text("Circulo")
+        }
+
+        Button (
+            onClick = {
+                mediaPlayer.start()
+            },
+            modifier = Modifier.padding(16.dp)
+        ){
+            Text("Play")
         }
     }
 }
